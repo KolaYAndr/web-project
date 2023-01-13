01c4699bac1b52e0bf102d34b86d66590dcf6d1e
KolaYAndr <nikolay_andreev02@mail.ru>
2023-01-13 15:35:41 +0300

    Обновил стили

--- src/main/java/repos/NoteRepo.java
+++ src/main/java/repos/NoteRepo.java
@@ -2,8 +2,7 @@ package repos;
 
 import models.Note;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface NoteRepo extends CrudRepository<Note, Long> {
+
 }
